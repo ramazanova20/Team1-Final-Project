215501dfb21692a529726e3c06b8832d3ce33dbd
Mahammad76 <mabdulazimoff@gmail.com>
2022-01-12 22:54:05 +0400

    Fields noted. Search function added

--- DataSet.java
+++ DataSet.java
@@ -5,13 +5,31 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 public class DataSet {
 	private List<HashMap<String, String>> data;
 	
 	public DataSet() {
-		data = new LinkedList<HashMap<String, String>>();
+		this.data = new LinkedList<HashMap<String, String>>();
+	}
+	
+	public void generateSample() {
+		HashMap<String, String> entry1 = new HashMap<String, String>();
+		HashMap<String, String> entry2 = new HashMap<String, String>();
+		HashMap<String, String> entry3 = new HashMap<String, String>();
+		
+		entry1.put(Fields.DATE, "4/4/1993");
+		entry1.put(Fields.LOCATION, "Off Barnegat, New Jersey");
+		entry1.put(Fields.TYPE, "Goodyear-Zeppelin U.S.S. Akron (airship)");
+		
+		entry2.put(Fields.DATE, "4/4/1993");
+		entry2.put(Fields.LOCATION, "asdsadasdas");
+		entry2.put(Fields.TYPE, "adsadada");
+		
+		this.data.add(entry1);
+		this.data.add(entry2);
 	}
 	
 	public void loadFromFile(String path) throws IOException {
@@ -24,6 +42,7 @@ public class DataSet {
 		StringTokenizer st = new StringTokenizer(firstLine, ",");
 		List<String> keys = new ArrayList<String>();
 		
+		
 		while(st.hasMoreTokens())
 			keys.add(st.nextToken());
 		
@@ -32,16 +51,68 @@ public class DataSet {
 		while((line = br.readLine()) != null) {
 			System.out.println(line);
 			st = new StringTokenizer(line, ",");
+			System.out.println(st.countTokens());
 			
 			HashMap<String, String> hm = new HashMap<String, String>();
 					
 			for(String key : keys)
 				hm.put(key, st.nextToken());
 			
-			data.add(hm);
+			this.data.add(hm);
 		}
 		
-			
+
 		
 	}
+	
+	public List<HashMap<String, String>> extractAll() {
+		return this.data;
+	}
+	
+	public List<HashMap<String, String>> extractFields(List<String> fields) {
+		List<HashMap<String, String>> newData = new LinkedList<HashMap<String, String>>();
+		
+		for(HashMap<String, String> entry :  data) {
+			HashMap<String, String> newEntry = new HashMap<String, String>();
+			
+			for(String key : entry.keySet()) {
+				if(fields.contains(key))
+					newEntry.put(key, entry.get(key));
+			}
+			
+			if(!newEntry.isEmpty())
+				newData.add(newEntry);
+		}
+		
+		return newData;
+	}
+	
+	public List<HashMap<String, String>> extractRange(int start, int end){
+		List<HashMap<String, String>> newData = new LinkedList<HashMap<String, String>>();
+		
+		for(int i = start - 1; i < end; i++) 
+			newData.add(data.get(i));
+			
+		return newData;
+	}
+	
+	public List<HashMap<String, String>> search(HashMap<String, String> sample){
+		List<HashMap<String, String>> newData = new LinkedList<HashMap<String, String>>();
+		
+		for(HashMap<String, String> entry : this.data) {
+			boolean found = true;
+			
+			for(String sampleKey : sample.keySet()) {
+				if(!sample.get(sampleKey).equals(entry.get(sampleKey)))
+					found = false;
+			}
+			
+			if(found)
+				newData.add(entry);
+		}
+		return newData;
+	}
+	
+	
+	
 }
---
+++ Fields.java
@@ -0,0 +1,23 @@
+
+public interface Fields {
+	String ID = "Id";
+	String DATE = "Date";
+	String TIME = "Time";
+	String LOCATION = "Location";
+	String OPERATOR = "Operator";
+	String FLIGHT = "Flight";
+	String ROUTE = "Route";
+	String TYPE = "Type";
+	String REGISTRATION = "Registration";
+	String CN_IN = "cn.In";
+	String ABOARD = "Aboard";
+	String FATALITIES = "Fatalities";
+	String GROUND = "Ground";
+	String SURVIVORS = "Survivors";
+	String SURVIVAL_RATE = "SurvivalRate";
+	String SUMMARY = "Summary";
+	String CLUST_ID = "ClustID";
+	
+	//Flight..	Route	Type	Registration	cn.In	Aboard	Fatalities	Ground	Survivors	SurvivalRate	Summary	ClustID
+
+}
